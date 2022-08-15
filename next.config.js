0196344d892aa03664b2b7c3a0336beba623c52a
Humayun Kabir <humayunkabir.cep@gmail.com>
2022-08-15 21:57:56 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProd ? '/' : '',
 };
 
 module.exports = nextConfig;
