f162e449805bd5027ae720fa57553e05d0a9cc8e
Humayun Kabir <humayunkabir.cep@gmail.com>
2022-08-15 21:54:34 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProd ? '/humayunkabir.dev/' : '',
 };
 
 module.exports = nextConfig;
