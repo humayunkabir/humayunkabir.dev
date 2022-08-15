a04a7f9c496b3e2a10d00f85af226d85fc669011
Humayun Kabir <humayunkabir.cep@gmail.com>
2022-08-15 16:03:22 +0600

    Initial commit

---
+++ out/_next/static/chunks/pages/components/Icon-2293e52a4a8b77f5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[590],{1909:function(a,b,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Icon",function(){return c(1830)}])}},function(a){a.O(0,[830,774,888,179],function(){var b;return a(a.s=1909)}),_N_E=a.O()}])
---
+++ pages/index.tsx
@@ -0,0 +1,52 @@
+import Link from 'next/link';
+import Icon from './components/Icon';
+
+export default function Home() {
+  return (
+    <main className='flex flex-col items-center justify-center w-screen min-h-screen py-8 text-center bg-[#14182c] text-white'>
+      <div className='max-w-3xl px-8 mx-auto'>
+        <h1 className='text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
+          A web engineer interested in design systems and closing the gap
+          between design and development
+        </h1>
+        <p className='mt-6'>
+          👋 Hey I&apos;m Humayun Kabir, a design-focused software engineer
+          based in Dhaka. I&apos;m currently working at bKash Limited as
+          Assistant Lead Engineer building the next-generation Mobile Financial
+          Services (MFS).
+        </p>
+
+        <div className='flex justify-center mt-16 space-x-4'>
+          <a
+            href='https://www.facebook.com/humayunkabirdev'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <Icon name='facebook' className='w-16 h-16' />
+          </a>
+          <a
+            href='https://www.linkedin.com/in/humayunkabirdev'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <Icon name='linkedin' className='w-16 h-16' />
+          </a>
+          <a
+            href='https://twitter.com/humayunkabirdev'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <Icon name='twitter' className='w-16 h-16' />
+          </a>
+          <a
+            href='https://github.com/humayunkabir'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <Icon name='github' className='w-16 h-16' />
+          </a>
+        </div>
+      </div>
+    </main>
+  );
+}
