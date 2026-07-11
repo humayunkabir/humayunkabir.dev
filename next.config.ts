6d6ca67d559695500ed2723b5f7337b047cf5f13
Humayun Kabir <humayunkabir.cep@gmail.com>
2026-07-12 00:48:24 +0600

    Fix github actions workflow for pnpm and Next.js 16

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
 	reactStrictMode: true,
 	typescript: { ignoreBuildErrors: true },
+	output: "export",
 	assetPrefix: isProd ? "/" : "",
 };
 
